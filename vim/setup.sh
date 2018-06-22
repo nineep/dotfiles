af4334c4a31f33f060debcf91c9ae05ed5bfd6f5
nineep <lunineep@gmail.com>
2018-06-22 18:12:35 +0800

    Replace vim NERDTree and tagbar window position, add init operation to vim/setup.sh

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+yum install cmake ctags -y
+
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     echo "Install vundle ..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 cp -v vimrc ~/.vimrc
 vim "+PluginInstall" "+x" "+x"
+
+python ~/.vim/bundle/YouCompleteMe/install.py
